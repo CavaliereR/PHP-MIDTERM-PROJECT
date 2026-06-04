0cff8a11ce29d51962b17081d5c283d970fcc8b4
lebard <Lino@NONO>
2026-06-04 20:03:47 +0800

    additional

--- main.php
+++ main.php
@@ -7,10 +7,6 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
 
-<?php
-header("Location: login.php");
-exit();
-?></head>
 
 <body class="bg-dark text-light">
 
